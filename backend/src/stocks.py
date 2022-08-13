deea31ea03ffe9880862335e4c9575806f61c54c
Alexander Tjhin <Alexandermichael123@yahoo.com>
2022-08-13 17:39:44 -0400

    format /stocks/id response

--- backend/src/stocks.py
+++ backend/src/stocks.py
@@ -3,13 +3,14 @@ import csv
 
 app = Flask(__name__)
 
-
-
 @app.route("/stocks/<id>", methods=["GET"])
 def get_stock(id):
     with open("../data/" + id + ".csv", mode="r") as csv_file:
-        reader = csv.reader(csv_file)
-        res = ""
+        reader = csv.DictReader(csv_file)
+        price = dict()
         for row in reader:
-            res += str(row) + "\n"
-        return res
+            price[row['Date']] = {k:v for (k,v) in row.items() if k != 'Date'}
+        return {
+            'id': id, 
+            'price': price,
+        }
