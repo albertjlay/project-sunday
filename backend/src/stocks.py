6146dbe29b6ddeed97819f6b616fded252d24d02
Alexander Tjhin <Alexandermichael123@yahoo.com>
2022-08-13 16:50:19 -0400

    Create /stocks/id endpoint
    Co-authored-by: Albert Lay <albertjlay@gmail.com>

--- backend/src/stocks.py
+++ backend/src/stocks.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import csv
 
 app = Flask(__name__)
 
-@app.route("/stocks")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+
+@app.route("/stocks/<id>", methods=["GET"])
+def get_stock(id):
+    with open("../data/" + id + ".csv", mode="r") as csv_file:
+        reader = csv.reader(csv_file)
+        res = ""
+        for row in reader:
+            res += str(row) + "\n"
+        return res
